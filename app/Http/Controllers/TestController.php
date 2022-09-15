e29d7c38aaebd80508c13202579ff9fbeb3da4a9
nex-finger <2020Tibakou@gmail.com>
2022-09-15 12:26:26 +0900

    ログイン情報管理ページ

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Test; //モデル(Test.php)の読み込み
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function index()
     {
-        return ('this is a test page.');
+        $test = new User;
+        
+        return view('test')->with(['tests' => $test->get()]);
     }
 }
